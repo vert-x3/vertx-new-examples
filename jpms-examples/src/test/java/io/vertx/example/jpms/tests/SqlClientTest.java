3848a7bec58efa3460c66e4e1411bac403942905
Julien Viet <julien@julienviet.com>
2024-10-25 17:21:42 +0200

    ok ok, docker only for linux in CI :-)

--- jpms-examples/src/test/java/io/vertx/example/jpms/tests/SqlClientTest.java
+++ jpms-examples/src/test/java/io/vertx/example/jpms/tests/SqlClientTest.java
@@ -20,7 +20,7 @@ import org.testcontainers.utility.DockerImageName;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
-@EnabledOnOs(value = { OS.LINUX, OS.MAC })
+@EnabledOnOs(value = { OS.LINUX })
 public class SqlClientTest {
 
   private static GenericContainer<?> container;
